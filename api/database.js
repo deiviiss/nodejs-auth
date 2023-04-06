38d2f007dc6b78df45c84d18c4061de7e9468858
David Hilera <david.hilera@hotmail.com>
2023-04-06 07:20:49 -0600

    build: :wrench: Add docker-compose for DB.
    Add docker-compose for DB. Use pnpm to manage packages.

--- api/database.js
+++ api/database.js
@@ -1,15 +1,17 @@
 import Mongoose from 'mongoose';
 
 const url = process.env.DATABASE_URL;
-
+console.log(url)
 if (!url) {
   throw new Error('DATABASE_URL is not set');
 }
 
 export const connect = async () => {
+  await Mongoose.set('strictQuery', true);
+
   await Mongoose.connect(url, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useUnifiedTopology: true
   });
 
   console.log('[db] Connected');
